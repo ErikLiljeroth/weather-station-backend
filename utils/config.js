f7181eab28382e86699c537e32a236cee6285946
Erik Liljeroth <liljeroth.erik@gmail.com>
2020-03-29 16:09:12 +0200

    small changes to utils/config

--- utils/config.js
+++ utils/config.js
@@ -4,9 +4,10 @@ let PORT = process.env.PORT
 let DB_HOST = process.env.DB_HOST
 let DB_PORT = process.env.DB_PORT
 let DB_DATABASE = process.env.DB_DATABASE
+let DB_TABLE = process.env.DB_TABLE
 let DB_USER = process.env.DB_USER
 let DB_PASSWORD = process.env.DB_PASSWORD
 
 module.exports = {
-  PORT, DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD
+  PORT, DB_HOST, DB_PORT, DB_DATABASE, DB_TABLE, DB_USER, DB_PASSWORD
 }
