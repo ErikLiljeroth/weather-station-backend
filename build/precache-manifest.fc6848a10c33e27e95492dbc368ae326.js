07cc3ad488b0167e747a41c7bad88591de8ad1d9
Erik Liljeroth <liljeroth.erik@gmail.com>
2020-04-03 10:07:14 +0200

    added missing static file in /build folder

---
+++ build/precache-manifest.fc6848a10c33e27e95492dbc368ae326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7e861397d180e7b706a9823ef8b9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61e721888f9801aaf3a",
+    "url": "/static/css/main.9c63c065.chunk.css"
+  },
+  {
+    "revision": "a127989297b3f7ae5faa",
+    "url": "/static/js/2.f11bfe4e.chunk.js"
+  },
+  {
+    "revision": "e64558e2f95ad6e9b52b3cecd9aea933",
+    "url": "/static/js/2.f11bfe4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61e721888f9801aaf3a",
+    "url": "/static/js/main.7bf7643e.chunk.js"
+  },
+  {
+    "revision": "385d09a378df49c10506",
+    "url": "/static/js/runtime-main.ef8a05d7.js"
+  },
+  {
+    "revision": "779caa76cbfc027a09b365a1a871a24f",
+    "url": "/static/media/GloriaHallelujah-Regular.779caa76.ttf"
+  }
+]);
