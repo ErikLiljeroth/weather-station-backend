3bffe22cf79548685d140f6fc2761ce1139b3ede
Erik Liljeroth <liljeroth.erik@gmail.com>
2020-06-09 11:13:44 +0200

    tuned pred

--- python/predictAndInsertSarima.py
+++ python/predictAndInsertSarima.py
@@ -159,13 +159,14 @@ class HandlePredictions():
         # Current best model:	SARIMAX([1, 8, 48], 1, [48])x(0, 1, [], 48)
         ar = [1, 8, 48]
         ma = [48]
-        self.model = SARIMAX(endog=train_data, order=(ar, 1, ma), seasonal_order=(0, 1, 0, 48), simple_differencing=True)
+        self.model = SARIMAX(endog=train_data, order=(ar, 1, ma), seasonal_order=(0, 1, 0, 48))
         self.model = self.model.fit(disp=True, low_memory=True) #start_params = [0.3, -0.04, 0.1, -0.9, 0.6])
 
     def predict(self, horizon = 12):
-        forecast = self.model.get_forecast(steps=12)
-        forecast = forecast.summary_frame(alpha=0.05)
-        predictions = forecast['mean'].values
+        forecast = self.model.forecast(steps=12)
+        #forecast = forecast.summary_frame(alpha=0.05)
+        #predictions = forecast['mean'].values
+        predictions = forecast
         return predictions
 
 
